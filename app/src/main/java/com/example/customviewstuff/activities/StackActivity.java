cbc215f669495aa59fe32d145b6d16436f897e49
wang.wenhui <294771275@qq.com>
2020-06-07 16:23:21 +0800

    完善汉诺塔

--- app/src/main/java/com/example/customviewstuff/activities/StackActivity.java
+++ app/src/main/java/com/example/customviewstuff/activities/StackActivity.java
@@ -1,10 +1,13 @@
 package com.example.customviewstuff.activities;
 
+import android.app.AlertDialog;
+
 import com.example.customviewstuff.BaseActivity;
 import com.example.customviewstuff.R;
 import com.example.customviewstuff.databinding.ActivityStackBinding;
 
 public class StackActivity extends BaseActivity<ActivityStackBinding> {
+    private AlertDialog exit;
 
     @Override
     protected int layoutId() {
@@ -13,6 +16,21 @@ public class StackActivity extends BaseActivity<ActivityStackBinding> {
 
     @Override
     protected void onInit() {
+        exit = new AlertDialog.Builder(this)
+                .setTitle("警告")
+                .setMessage("是否退出？")
+                .setPositiveButton("确定", (dialog, which) -> {
+                    exit.dismiss();
+                    finish();
+                })
+                .setNegativeButton("取消", (dialog, which) -> {
+                    exit.dismiss();
+                })
+                .create();
+    }
 
+    @Override
+    public void onBackPressed() {
+        exit.show();
     }
 }
--- app/src/main/java/com/example/customviewstuff/customs/StackView.java
+++ app/src/main/java/com/example/customviewstuff/customs/StackView.java
@@ -3,14 +3,21 @@ package com.example.customviewstuff.customs;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.graphics.Shader;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.SurfaceHolder;
 
 import com.example.customviewstuff.helpers.Stack;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Created by Wang.Wenhui
@@ -24,11 +31,17 @@ public class StackView extends BaseSurfaceView {
     private Block selected;
     private int owner;
     private float baseLength, blockHeight;
-    private int level;
-    private static final long moveTime = 100;
+    private int level, step;
+    private static final long moveTime = 300;
     private RectF drawRect;
     private Random random;
     private float topY;
+    private LinearGradient red, green;
+    private Runnable nextLevel = () -> {
+        canTouch = true;
+        resetLevel(++level);
+    };
+    private List<Block> pool;
 
     public StackView(Context context) {
         super(context);
@@ -48,23 +61,29 @@ public class StackView extends BaseSurfaceView {
         rects = new RectF[3];
         bxs = new float[3];
         random = new Random();
+        pool = new CopyOnWriteArrayList<>();
     }
 
     @Override
     protected void onReady() {
         int w = getMeasuredWidth();
         int h = getMeasuredHeight();
-        rects[0] = new RectF(0, h / 3f, w / 3f, h);
+        float containerH = h * 0.4f;
+        rects[0] = new RectF(0, containerH, w / 3f, h);
         bxs[0] = (rects[0].left + rects[0].right) / 2;
-        rects[1] = new RectF(w / 3f, h / 3f, w / 3f * 2, h);
+        rects[1] = new RectF(w / 3f, containerH, w / 3f * 2, h);
         bxs[1] = (rects[1].left + rects[1].right) / 2;
-        rects[2] = new RectF(w / 3f * 2, h / 3f, w, h);
+        rects[2] = new RectF(w / 3f * 2, containerH, w, h);
         bxs[2] = (rects[2].left + rects[2].right) / 2;
-        topY = h / 3.2f;
+        red = new LinearGradient(0, h, 0, rects[0].top, new int[]{0xAAFF0000, Color.TRANSPARENT}, null, Shader.TileMode.CLAMP);
+        green = new LinearGradient(0, h, 0, rects[0].top, new int[]{0xAA00FF00, Color.TRANSPARENT}, null, Shader.TileMode.CLAMP);
+        topY = h * 0.35f;
         drawRect = new RectF();
         baseLength = w * 0.3f;
         blockHeight = w * 0.03f;
-        resetLevel(1);
+        if (level == 0) {
+            resetLevel(1);
+        }
         startAnim();
     }
 
@@ -82,9 +101,11 @@ public class StackView extends BaseSurfaceView {
 
     @Override
     protected void onRefresh(Canvas canvas) {
+        canvas.drawColor(Color.WHITE);
+        drawLevel(canvas);
         drawContainer(canvas);
         for (Stack<Block> stack : stacks) {
-            drawStackL(canvas, stack);
+            drawStack(canvas, stack);
         }
         if (selected != null) {
             mPaint.setColor(selected.color);
@@ -92,10 +113,25 @@ public class StackView extends BaseSurfaceView {
             float hh = blockHeight / 2;
             drawRect.set(selected.x - hw, selected.y - hh, selected.x + hw, selected.y + hh);
             canvas.drawRoundRect(drawRect, blockHeight, blockHeight, mPaint);
+            if (selected.x == selected.tx) {
+                mPaint.setColor(0xCCCCCCCC);
+                canvas.drawLine(selected.x - hw, selected.y, selected.x - hw, getMeasuredHeight(), mPaint);
+                canvas.drawLine(selected.x + hw, selected.y, selected.x + hw, getMeasuredHeight(), mPaint);
+            }
         }
     }
 
-    private void drawStackL(Canvas canvas, Stack<Block> stack) {
+    private void drawLevel(Canvas canvas) {
+        mPaint.setTextSize(getMeasuredWidth() * 0.16f);
+        mPaint.setColor(Color.BLACK);
+        mPaint.setTextAlign(Paint.Align.CENTER);
+        canvas.drawText("第" + level + "关", getMeasuredWidth() >> 1, getMeasuredHeight() * 0.14f, mPaint);
+
+        mPaint.setTextSize(getMeasuredWidth() * 0.05f);
+        canvas.drawText("共移动了" + step + "步", getMeasuredWidth() >> 1, getMeasuredHeight() * 0.2f, mPaint);
+    }
+
+    private void drawStack(Canvas canvas, Stack<Block> stack) {
         for (Block block : stack) {
             mPaint.setColor(block.color);
             float hw = block.length / 2;
@@ -106,9 +142,18 @@ public class StackView extends BaseSurfaceView {
     }
 
     private void drawContainer(Canvas canvas) {
-        mPaint.setColor(Color.WHITE);
+        mPaint.setColor(Color.BLACK);
         canvas.drawLine(rects[1].left, rects[1].bottom, rects[1].left, rects[1].top, mPaint);
         canvas.drawLine(rects[1].right, rects[1].bottom, rects[1].right, rects[1].top, mPaint);
+        if (selected != null && inContainer != -1) {
+            mPaint.setShader(canDrop() ? green : red);
+            canvas.drawRect(rects[inContainer], mPaint);
+            mPaint.setShader(null);
+        }
+    }
+
+    private boolean canDrop() {
+        return stacks[inContainer].size() == 0 || stacks[inContainer].top().length > selected.length;
     }
 
     @Override
@@ -122,21 +167,38 @@ public class StackView extends BaseSurfaceView {
     }
 
     @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        super.surfaceDestroyed(holder);
+        removeCallbacks(nextLevel);
+    }
+
+    @Override
     protected boolean preventClear() {
         return false;
     }
 
     public void resetLevel(int level) {
+        if (level > 10) {
+            level = 10;
+        }
         this.level = level;
         for (Stack<Block> stack : stacks) {
             stack.clear();
         }
         float len = baseLength * 0.1f, y = getMeasuredHeight() - blockHeight / 2;
         for (int i = 0; i < level; i++) {
-            Block block = new Block(bxs[0], y, baseLength - len * i);
+            Block block;
+            if (pool.size() > 0) {
+                block = pool.remove(0);
+                block.reset(bxs[0], y, baseLength - len * i);
+            } else {
+                block = new Block(bxs[0], y, baseLength - len * i);
+            }
             y -= blockHeight;
             stacks[0].pull(block);
         }
+        pool.addAll(stacks[0]);
+        Collections.shuffle(pool);
     }
 
     private class Block {
@@ -146,8 +208,13 @@ public class StackView extends BaseSurfaceView {
         int color;
 
         Block(float x, float y, float length) {
-            this.tx = this.x = x;
-            this.ty = this.y = y;
+            set(x, y);
+            this.length = length;
+            color = randomColor();
+        }
+
+        void reset(float x, float y, float length) {
+            set(x, y);
             this.length = length;
             color = randomColor();
         }
@@ -163,26 +230,35 @@ public class StackView extends BaseSurfaceView {
                     y = ty;
                 }
                 if (x == tx && y == ty && stacks[2].size() == level) {
-                    resetLevel(++level);
+                    canTouch = false;
+                    postDelayed(nextLevel, 500);
                 }
             }
         }
 
         void set(float newX, float newY) {
-            this.tx = newX;
-            this.ty = newY;
-            incX = (tx - x) * UPDATE_RATE / moveTime;
-            incY = (ty - y) * UPDATE_RATE / moveTime;
+            if (tx != newX || ty != newY) {
+                this.tx = newX;
+                this.ty = newY;
+                incX = (tx - x) * UPDATE_RATE / moveTime;
+                incY = (ty - y) * UPDATE_RATE / moveTime;
+            }
         }
 
     }
 
     private int randomColor() {
-        return Color.rgb(random.nextInt(255), random.nextInt(255), random.nextInt(255));
+        return Color.rgb(random.nextInt(225), random.nextInt(225), random.nextInt(225));
     }
+
+    private volatile int inContainer = -1;
+    private boolean canTouch = true;
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        if (!canTouch) {
+            return false;
+        }
         float eventX = event.getX();
         float eventY = event.getY();
         int whichContainer = inWhichContainer(eventX, eventY);
@@ -190,6 +266,7 @@ public class StackView extends BaseSurfaceView {
             case MotionEvent.ACTION_DOWN:
                 if (whichContainer != -1 && !stacks[whichContainer].isEmpty()) {
                     owner = whichContainer;
+                    inContainer = whichContainer;
                     selected = stacks[whichContainer].pop();
                     selected.set(selected.x, topY);
                 }
@@ -197,17 +274,23 @@ public class StackView extends BaseSurfaceView {
             case MotionEvent.ACTION_MOVE:
                 if (selected != null && whichContainer != -1) {
                     selected.set(bxs[whichContainer], selected.ty);
+                    inContainer = whichContainer;
                 }
                 break;
             case MotionEvent.ACTION_UP:
             case MotionEvent.ACTION_CANCEL:
-                if (selected != null && whichContainer != -1) {
+                if (selected != null && inContainer != -1) {
+                    whichContainer = inContainer;
+                    inContainer = -1;
                     Stack<Block> newStack = stacks[whichContainer];
                     Stack<Block> ownerStack = stacks[owner];
                     if (newStack.isEmpty() || newStack.top().length > selected.length) {
                         setXY(selected, newStack, bxs[whichContainer]);
                         newStack.pull(selected);
                         selected = null;
+                        if (owner != whichContainer) {
+                            step++;
+                        }
                     } else {
                         setXY(selected, ownerStack, bxs[owner]);
                         ownerStack.pull(selected);
